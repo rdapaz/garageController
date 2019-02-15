b6373db25fe6b7ed03cc50908f88322829d0a74e
Ricardo da Paz <rdapaz@our.ecu.edu.au>
2019-02-16 07:34:59 +0800

    Still not working
    Split up MQTT functionality from garage class, but still no cigarr

--- controller.py
+++ controller.py
@@ -4,20 +4,11 @@ import time
 import logging
 import datetime
 
+# This is a test
 
 class GarageDoorController:
 
     def __init__(self):
-
-        def on_message(client, userdata, message):
-            print('Got a message...')
-            if message.topic == 'garage/command':
-                if message.payload.decode() in ('open', 'close'):
-                    self.toggleGarage()
-
-        def on_connect(client, userdata, flags, rc):
-            print("Connected With Result Code {}".format(rc))
-            self.client1.subscribe('garage/command')
 
         GPIO.setmode(GPIO.BCM)
         # Private properties
@@ -26,23 +17,18 @@ class GarageDoorController:
         self.RELAY_OUT = 24
         self.DELAY = 0.25
         self.ERROR = 0.05
-        self.MQTT_SERVER = '10.11.12.118'
 
         # Public properties
         self.distance = 0.0
         self.status = None
         self.lastStatus = None
-        self.client1 = mqtt.Client('garage_controller_1')
-        self.client2 = mqtt.Client('garage_controller_2')
-        self.client1.on_connect = on_connect
-        self.client1.on_message = on_message
-        self.client1.connect(self.MQTT_SERVER, port=1883)
 
+        GPIO.setwarnings(False)
         GPIO.setup(self.TRIGGER, GPIO.OUT)
         GPIO.setup(self.ECHO, GPIO.IN)
         GPIO.setup(self.RELAY_OUT, GPIO.OUT)
 
-    def checkIfOpen(self):
+    def scanGarageStatus(self):
         """
             Checks whether the garage is open or closed
         """
@@ -62,26 +48,7 @@ class GarageDoorController:
         else:
             self.status = "Open"
 
-        if self.status != self.lastStatus:
-            self.statusChange = True
-        else:
-            self.statusChange = False
-
-        self.lastStatus = self.status
-
-    def publishStatus(self):
-        """
-            Publish status via MQTT
-        """
-
-        if self.statusChange:
-            print(self.status)
-            self.client2.connect(self.MQTT_SERVER, port=1883)
-            self.client2.publish(topic='garage/status',
-                                 payload=self.status,
-                                 qos=0,
-                                 retain=False)
-            self.client2.disconnect()
+        return self.status
 
     def toggleGarage(self):
         print('Garage would have opened')
@@ -89,19 +56,54 @@ class GarageDoorController:
         # time.sleep(DELAY)
         # GPIO.output(RELAY_OUT, GPIO.LOW)
 
-    def run(self):
-
-        while True:
-            try:
-                self.checkIfOpen()
-                self.publishStatus()
-                time.sleep(2)
-                self.client1.loop()
-            except KeyboardInterrupt:
-                GPIO.cleanup()
-                self.client1.loop_stop()
-
 
 if __name__ == '__main__':
+    
+    MQTT_SERVER = '10.11.12.118'
+    
+    lastStatus = False
+    garageStatus = ''
+    global toggleGarage
+    toggleGarage = False
+
     g = GarageDoorController()
-    g.run()
+    
+
+    def on_connect(client, userdata, flags, rc):
+        print("Connected With Result Code {}".format(rc))
+        client.subscribe('garage/command')
+    
+    def on_message(client, userdata, message):
+        print(str(message.payload))
+        if message.topic == 'garage/command':
+            if message.payload.decode() in ('open', 'close'):
+                toggleGarage = True
+            else:
+                toggleGarage = False
+    
+    client = mqtt.Client('garage_controller')
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(MQTT_SERVER, 1883, 60)
+    client.loop_start()
+    time.sleep(1)
+    
+
+    while True:
+        try:
+            garageStatus = g.scanGarageStatus()
+            if garageStatus != lastStatus:
+                print(garageStatus)
+                client.publish(topic='garage/status',
+                               payload=garageStatus,
+                               qos=0,
+                               retain=False)
+                lastStatus = garageStatus
+
+            if toggleGarage:
+                g.toggleGarage()
+            time.sleep(5)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            client.loop_stop()
+            client.disconnect()
